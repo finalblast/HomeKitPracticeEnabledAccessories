e6994cb1949c0cbfa45720ae49d0d0d84580310c
Nam (Nick) N. HUYNH <nick@enclave.vn>
2016-03-18 17:05:21 +0700

    HomeKit - Add a home to home manager, add a room to home, searching accessories, add accessories to home, remove accessories, assign accessory to room and print them out.

--- HomeKitPracticeEnabledAccessories/ViewController.swift
+++ HomeKitPracticeEnabledAccessories/ViewController.swift
@@ -7,12 +7,64 @@
 //
 
 import UIKit
+import HomeKit
 
 class ViewController: UIViewController {
 
+    var accessories = [HMAccessory]()
+    var home: HMHome!
+    var room: HMRoom!
+    
+    lazy var accessoryBrowser: HMAccessoryBrowser = {
+        
+       let browser = HMAccessoryBrowser()
+        browser.delegate = self
+        return browser
+        
+    }()
+    
+    var randomHomeName: String = {
+        
+        return "Home \(arc4random_uniform(UInt32.max))"
+        
+    }()
+    
+    let roomName = "Bedroom 1"
+    
+    var homeManager: HMHomeManager!
+
+    // MARK: Finding services for accessry
+    
+    func findServicesForAccessory(accessory: HMAccessory!) {
+        
+        println("Finding services ...")
+        for service in accessory.services as [HMService] {
+            
+            println("Service Name: \(service.name)")
+            println("Service Type: \(service.serviceType)")
+            findCharacteristicOfService(service)
+            
+        }
+        
+    }
+    
+    // MARK: Finding characteristic of service
+    
+    func findCharacteristicOfService(service: HMService!) {
+        
+        println("Finding characteristic ...")
+        for characteristic in service.characteristics as [HMCharacteristic] {
+            
+            println("Chracteristic Type: \(characteristic.characteristicType)")
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        homeManager = HMHomeManager()
+        homeManager.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +75,75 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: HMHomeManagerDelegate, HMAccessoryBrowserDelegate {
+    
+    func homeManagerDidUpdateHomes(manager: HMHomeManager!) {
+        
+        manager.addHomeWithName(randomHomeName, completionHandler: { (home, error) -> Void in
+            
+            if error != nil {
+                
+                println("Could not add home! \(error.description)")
+                return
+                
+            }
+            
+            println("Added Home")
+            self.home = home
+            home.addRoomWithName(self.roomName, completionHandler: { (room, error) -> Void in
+                
+                if error != nil {
+                    
+                    println("Could not add room to home! \(error.description)")
+                    return
+                    
+                }
+                
+                println("Added Room")
+                self.room = room
+                self.accessoryBrowser.startSearchingForNewAccessories()
+                
+            })
+            
+        })
+        
+    }
+    
+    func accessoryBrowser(browser: HMAccessoryBrowser!, didFindNewAccessory accessory: HMAccessory!) {
+        
+        println("Found new accessory")
+        home.addAccessory(accessory, completionHandler: { (error) -> Void in
+            
+            if error != nil {
+                
+                println("Could not add accessory to home!")
+                return
+                
+            }
+            
+            println("Added Accessory")
+            self.home.assignAccessory(accessory, toRoom: self.room, completionHandler: { (error) -> Void in
+                
+                if error != nil {
+                    
+                    println("Could not assign accessory to room!")
+                    return
+                    
+                }
+                
+                println("Assigned to room: \(self.room.name)")
+                self.findServicesForAccessory(accessory)
+                
+            })
+            
+        })
+        
+    }
+    
+    func accessoryBrowser(browser: HMAccessoryBrowser!, didRemoveNewAccessory accessory: HMAccessory!) {
+        
+        println("\(accessory) has been removed!")
+        
+    }
+    
+}
